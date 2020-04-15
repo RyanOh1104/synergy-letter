def5935f2e0ac240bf4b69cd608e37334803eda5
Ryan <yyjo1104@naver.com>
2020-04-15 16:47:53 +0900

    minor change

--- synergy/wsgi.py
+++ synergy/wsgi.py
@@ -15,5 +15,6 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'synergy.settings')
 
 application = get_wsgi_application()
 
-from whitenoise.django import DjangoWhiteNoise
-application = DjangoWhiteNoise(application)
+# from whitenoise.django import DjangoWhiteNoise
+# application = DjangoWhiteNoise(application)
+
