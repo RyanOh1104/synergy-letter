49255a739f2fae4c3a13ba13a9d42d6711c0e9c9
Ryan <yyjo1104@naver.com>
2020-04-15 16:51:11 +0900

    minor change

--- main/urls.py
+++ main/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from main import views
 
 urlpatterns = [
-    path('home/', views.home, name='home'),
+    path('', views.home, name='home'),
     path('bookmain/<int:id>', views.bookmain, name='bookmain'),
     path('letter/<str:title>/<int:id>', views.letter, name="letter"),
 ]
